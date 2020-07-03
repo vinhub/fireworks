86a95fc9782d1df5a75b8713aaaab674c818dd0a
vinhub <vin@introp.net>
2020-07-03 11:28:27 -0700

    init personalize form using url params

--- js/form.js
+++ js/form.js
@@ -1,6 +1,19 @@
 function openPersonalizeForm() {
   closeAboutPopup();
   document.getElementById('personalizeForm').style.display = 'block';
+  
+  const urlParams = new URLSearchParams(window.location.search);
+  var name = urlParams.get('name');
+  var message = urlParams.get('message');
+
+  if (!name)
+    name = 'Vin Bhalerao';
+
+  if (!message)
+    message = 'Happy Independence Day!';
+    
+  document.getElementById('name').value = name;
+  document.getElementById('message').value = message;
 }
 
 function closePersonalizeForm() {
